54022342668a0b546aab63e0426ebebc8841ba60
Ramadhan Febri Utama <rafeta02@student.uns.ac.id>
2023-12-08 08:53:49 +0700

    coba cache false ajaz

--- resources/views/admin/deliveryOrders/create.blade.php
+++ resources/views/admin/deliveryOrders/create.blade.php
@@ -130,7 +130,7 @@
                             results: data
                         };
                     },
-                    cache: true
+                    cache: false
                 }
         });
 
